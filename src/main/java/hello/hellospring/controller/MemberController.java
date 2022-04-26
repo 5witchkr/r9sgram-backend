43c55ec3661bdb83be0b85515c8d975170e9a79c
5witchkr <hob4419@gmail.com>
2022-04-26 23:29:23 +0900

    postmember

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,6 +15,22 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+    //join page
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "hello";
+    }
+
+    //join
+    @PostMapping("members/new")
+    //form에 입력.. 지금은 안만들어둠
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
     }
 
 }
